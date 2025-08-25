7bfee6138bec562684f37ebcd312ca38218db9a6
Piyush Patil <piyushpatil1741@gmail.com>
2025-08-26 03:54:40 +0530

    Episode 3, Learning JSX.

--- ep-1/App.js
+++ ep-1/App.js
@@ -1,6 +1,21 @@
-        const heading  = React.createElement("h1",{},"Namaskar Sablog!")
-        console.log(heading)
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 
-        const root = ReactDOM.createRoot(document.getElementById("root"))
 
-        root.render(heading)
+const heading  = <h1
+ id="title" key = "h1" >
+        Namaste React whoa I am writing this in JSX!
+ </h1>;
+
+ const HeaderComponent = () => {
+        return (
+                <div>
+        <h1>Hello World from Functional Component</h1>
+        <h2>This is it's description</h2>        
+
+        </div>
+        )
+ }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent/>);
